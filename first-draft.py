9f1ab8c15f065e3bf57d576ba8a33793f06ce01b
Sandeep Ravindra <sandeeprds95@gmail.com>
2016-01-18 11:13:16 +0530

    files renamed

---
+++ first-draft.py
@@ -0,0 +1,43 @@
+#Scrapping the ZOHO Website for internals and attendance marks. 3rd year CSE only!
+
+from bs4 import BeautifulSoup
+import urllib.request
+import webbrowser
+
+BASE_URL = 'https://creator.zohopublic.com/srm_university/attendance-2015-16/view-perma/Student_Status/H2rQs63qk22E9xdk1PAnBnNqzuFfgZMtD9yN5RkfuA7V6RsCdOfxxXeXjud80upTEZgQxjPJR3b0ffxU49rYOSr7fpaa9g1hRZmd/studentID=27276430000054'
+
+def get_user_by_name(name):
+    for i in range(79227,82151,4):
+        x = urllib.request.urlopen(BASE_URL + str(i))
+        soup = BeautifulSoup(x.read(),'html.parser')
+        rows = soup.find("table",border=0).find_all('tr')
+        col = rows[2].find_all('td')
+        res = col[5].get_text()
+        if(res == name):
+            new = 2
+            url = BASE_URL + str(i)
+            webbrowser.open(url,new=new)
+            return;
+    print (name + '- Not found!')
+
+def get_user_by_regno(regno):
+    for i in range(79227,82151,4):
+        x = urllib.request.urlopen(BASE_URL + str(i))
+        soup = BeautifulSoup(x.read(),'html.parser')
+        rows = soup.find("table",border=0).find_all('tr')
+        col = rows[2].find_all('td')
+        res = col[2].get_text()
+        if(res == regno):
+            new = 2
+            url = BASE_URL + str(i)
+            webbrowser.open(url,new=new)
+            return;
+    print (name + '- Not found!')
+        
+search =  int(input("How do you want to search? Register Number(1) or Name(2)?"))
+if(search == 1):
+    regno = str(input("Enter the Register Number:"))
+    get_user_by_regno(regno);
+else:
+    name = str(input("Enter the name:"))
+    get_user_by_name(name);
